7d87b9e2b542f789b9bd1d24343a617c4efe5ec2
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2025-10-25 00:08:13 +0530

    feat: consume config module in database module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,5 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
-@Module({ imports: [MongooseModule.forRoot('mongodb://127.0.0.1/adydb')] })
+// TODO - add zod schema for env files
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      // forRootAsync makes usre these dependencies are loaded before this module loads
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService], // inject the services that are being used in the factory
+    }),
+  ],
+})
 export class DatabaseModule {}
