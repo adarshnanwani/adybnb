031462bc26ffe6581d87daa32e5ed23c3378f5ef
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2025-10-27 16:44:10 +0530

    feat: add validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDate({ message: 'Invalid date format' })
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate({ message: 'Invalid date format' })
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
