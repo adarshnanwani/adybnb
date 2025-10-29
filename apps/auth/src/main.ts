3386ac3271e409e2cfb99db0920f016555d61d91
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2025-10-29 20:40:08 +0530

    feat: enable validation and logging for auth module

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // whitelist: true, strips away the properties which don't exist in the model
+  app.useLogger(app.get(Logger));
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
